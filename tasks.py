c5b34e04d74004e7f0b5330b8695aec2eafd049b
AleksandrBelyan <belyan.alex93@gmail.com>
2023-02-21 11:48:34 +0300

    fix typing in task1, task3

--- tasks.py
+++ tasks.py
@@ -1,7 +1,7 @@
 from exceptions import TaskException
 
 
-def task1(x: [float, int], y: [float, int]) -> int:
+def task1(x: [float, int], y: [float, int]) -> [float, int]:
     """
     Даны действительные числа x и y.
     Вернуть (|x| − |y|) / (1+ |xy|)
@@ -15,7 +15,7 @@ def task2(a: [float, int]) -> tuple[[float, int], [float, int]]:
     """
 
 
-def task3(a: [float, int], b: [float, int]) -> int:
+def task3(a: [float, int], b: [float, int]) -> [float, int]:
     """
     Даны два катета прямоугольного треугольника.
     Вернуть длину гипотенузы.
